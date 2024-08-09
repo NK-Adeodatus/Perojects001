e81278802d401ce89cdccc1e18433af4741f8bbe
Nkundimana Adeodatus <nkundiman3@gmail.com>
2024-08-09 14:02:01 +0200

    my working web app

--- index.js
+++ index.js
@@ -1,7 +1,39 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
+import { getDatabase,
+    ref,
+    push,
+    onValue
+ } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-database.js";
+
+
+const firebaseConfig = {
+    projectId: "my-firebase-project-4dafb",
+    databaseURL: "https://my-firebase-project-4dafb-default-rtdb.firebaseio.com/"
+}
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app)
+const referenceInDB = ref(database,"messages")
+
 let inputChat=document.getElementsByClassName("input-el")[0]
 let ulel = document.getElementsByClassName("ul-el")[0]
 let chatbtn = document.getElementById("chat-btn")
 chatbtn.addEventListener("click", function(){
-    ulel.innerHTML+="<div>"+inputChat.value+"</div>"
-    inputChat.value = ""
+    const message = inputChat.value.trim()
+    if(message){
+        push(referenceInDB,{content: message, timestamp: Date.now()})
+        ulel.innerHTML+="<div>"+inputChat.value+"</div>"
+        inputChat.value = ""
+    }
+    
+
+})
+onValue(referenceInDB,function(snapshot){
+    const snapshotValues = snapshot.val()
+    ulel.innerHTML = ""
+    let sms = Object.entries(snapshotValues)
+    for(let i=0;i<=sms.length-1;i++){
+        let messageContent = sms[i][1].content;
+        ulel.innerHTML+="<div>"+messageContent+"</div>"
+    }
+    
 })
