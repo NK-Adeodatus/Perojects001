9c0df2e5cbd7a059b95fe30164cf2cdb5b5e58c5
Nkundimana Adeodatus <nkundiman3@gmail.com>
2024-10-15 12:49:33 +0200

    added the necessary the functionalities

--- index.js
+++ index.js
@@ -47,22 +47,44 @@ onValue(referenceInDB,function(snapshot){
     for(let i=0;i<=sms.length-1;i++){
         let messageContent = sms[i][1].content;
         let messageUser = sms[i][1].user
+        let messageTimestamp = new Date(sms[i][1].timestamp).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }); // Format to show only hour and minute
         if(messageContent){
             
             if(messageUser === currentUser){
+                // Create a header div for the username and timestamp container
+                const headerContainer = document.createElement("div");
+                headerContainer.classList.add("m-header-container")
+                // Create a div for timestamp
+                const timestampDiv = document.createElement("div");
+                timestampDiv.textContent = messageTimestamp; // Set the timestamp text
+                timestampDiv.classList.add("timestamp")
                 // Create a new div for the user's name
                 const userNameDiv = document.createElement("div");
                 userNameDiv.textContent = messageUser; // Set the text to the message sender's name
                 userNameDiv.classList.add("m-user-name")
-                ulel.appendChild(userNameDiv)
+                // Append username and timestamp divs to the header container
+                headerContainer.appendChild(userNameDiv);
+                headerContainer.appendChild(timestampDiv);
+                ulel.appendChild(headerContainer)
+                
                 ulel.innerHTML+=`<div class="my-message">${messageContent}</div>`
         }
         else{
+            // Create a header div for the username and timestamp container
+            const headerContainer = document.createElement("div");
+            headerContainer.classList.add("o-header-container")
+            // Create a div for timestamp
+            const timestampDiv = document.createElement("div");
+            timestampDiv.textContent = messageTimestamp; // Set the timestamp text
+            timestampDiv.classList.add("timestamp")
             // Create a new div for the user's name
             const userNameDiv = document.createElement("div");
             userNameDiv.textContent = messageUser; // Set the text to the message sender's name
             userNameDiv.classList.add("o-user-name")
-            ulel.appendChild(userNameDiv)
+            // Append username and timestamp divs to the header container
+            headerContainer.appendChild(userNameDiv);
+            headerContainer.appendChild(timestampDiv);
+            ulel.appendChild(headerContainer)
             ulel.innerHTML+=`<div class="other-message">${messageContent}</div>` 
         } 
         }
